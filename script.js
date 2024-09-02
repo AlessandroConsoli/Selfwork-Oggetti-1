4c60fc2475ad51d68b2cf5161482d15d32dd4808
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-09-02 18:12:12 +0200

    completato

---
+++ script.js
@@ -0,0 +1,11 @@
+let alessandro = {
+    'nome' : 'Alessandro',
+    'cognome' : 'Consoli',
+    'età' : 42,
+    'saluta' : function() {
+        console.log('Ciao a tutti da Alessandro!');
+        
+    }
+};
+
+alessandro.saluta();
